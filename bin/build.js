f2cf8b373b3c6278fef021292bbded766b630035
kubosho <ta2@o2p.jp>
2017-04-19 13:41:01 +0900

    build(dist): add formatter

--- bin/build.js
+++ bin/build.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const postcss = require("postcss")
 const atImport = require("postcss-import")
 const comments = require("postcss-discard-comments");
+const stylefmt = require("stylefmt");
 
 const cwd = process.cwd()
 const inputFile = `${cwd}/main.css`
@@ -13,6 +14,7 @@ const css = fs.readFileSync(inputFile, "utf8")
 postcss()
   .use(atImport())
   .use(comments())
+  .use(stylefmt())
   .process(css, { from: inputFile })
   .then((result) => {
     fs.writeFileSync(outputFile, result.css)
